7075a3c9754f3e696493da97475c609fb87980cd
mohamedo396 <mohamadamrani83@gmail.com>
2025-03-18 13:20:34 +0000

    set the things up

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,17 @@
-import { Router } from "express";
+import { Router } from 'express';
+
+import { getUser, getUsers } from '../controllers/user.controller.js'
 
 const userRouter = Router();
 
+userRouter.get('/', getUsers);
 
-userRouter.get("/", (req, res) => {res.send("fetch all users")});
-userRouter.get("/:id", (req, res) => {res.send("fetch user by id")});
-userRouter.post("/", (req, res) => {res.send("create user")});
-userRouter.put("/:id", (req, res) => {res.send("update user by id")});
-userRouter.delete("/:id", (req, res) => {res.send("delete user by id")});
+userRouter.get('/:id', getUser);
+
+userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
+
+userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
+
+userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
 
 export default userRouter;
